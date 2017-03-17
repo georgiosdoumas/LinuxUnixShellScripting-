44862a40de53290e666f40b55eb96e43c079dd9f
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-03-17 23:09:39 +0100

    Update test1_trap.sh

--- Linux.Command.Line.And.ShellScripting.Bible.2015.3rdEd/chapter16/test1_trap.sh
+++ Linux.Command.Line.And.ShellScripting.Bible.2015.3rdEd/chapter16/test1_trap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+## !/usr/bin/ks  # this should be the 1st line of a korn shell script
 # Testing signal trapping
 # trap "echo ' Sorry but I am not going to finish! I have trapped Ctrl-C'" SIGINT
 # or equivalently : 
@@ -6,7 +7,7 @@
 # another multi-command example is : 
 # trap " echo 'You pressed Ctrl-C! ' > ch16output_trap ; exit" SIGINT
 # but to make the following work I must reverse the use of ' and " 
- trap 'tempfile=$(mktemp ch16output1.XXXXXX) ; echo "You force me to finish with Ctrl-C,on loop #$count ">$tempfile ; exit' SIGINT
+trap 'tempfile=$(mktemp ch16output1.XXXXXX); echo "You force me to finish with Ctrl-C,on loop #$count ">$tempfile ; exit' SIGINT
 echo "This is a test script"
 count=1
 while [ $count -le 10 ]
@@ -14,6 +15,6 @@ do
   echo "Loop #$count"
   sleep 1
    count=$[ $count + 1 ]  ## arithmetics for bash 
-  ## count=$(( $count + 1 ))   # arithmetics for ksh
+  ## count=$(( $count + 1 ))   ## arithmetics for ksh, in case you develop a ksh
 done
 echo "This is the end of the test script"
